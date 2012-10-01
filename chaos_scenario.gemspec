9caa1938eed09e07f3d38baaae1cda185d268893
Paul Meskers <paul@paulmeskers.com>
2012-09-30 22:59:23 -0400

    Fix gemspec & old ruby code

--- chaos_scenario.gemspec
+++ chaos_scenario.gemspec
@@ -3,6 +3,7 @@ Gem::Specification.new do |s|
   s.version     = "0.1"
   s.authors     = ["Paul Meskers"]
   s.email       = ["paul@paulmeskers.com"]
+  s.files       = `git ls-files`.split("\n")
   s.summary     = "Generate random scenarios or plots."
 
   s.license     = "MIT"
--- lib/chaos_scenario/generator.rb
+++ lib/chaos_scenario/generator.rb
@@ -3,13 +3,12 @@ module ChaosScenario
     extend self
 
     def create_scenario
-      puts "Creating scenario"
       puts "A #{protagonist} #{placement} #{location} #{goal}"
     end
 
     def method_missing classifier
       list = YAML.load_file("#{ROOT_PATH}/data/#{classifier}s.yml")
-      list["#{classifier}s"].choice
+      list["#{classifier}s"].sample
     end
 
   end
